34863fa4f082a97af07194419911cb6176876cbf
Carl Ruiz <carl.45.cr@gmail.com>
2016-08-16 00:15:42 +0200

    all stepd modules oks

--- app.js
+++ app.js
@@ -1,50 +1,41 @@
-var express = require('express')
 var MongoClient = require('mongodb').MongoClient;
-var ObjectID = require('mongodb').ObjectID;
-var jade = require('jade')
-var bodyParser = require('body-parser')
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var bodyParser = require('body-parser');
+
+var routerTasks = require('./routes/tasks');
+var PORT = process.env.PORT || 3000;
+
 var app = express();
-var url = 'mongodb://localhost:27017/tasks';
-var publicFolder = __dirname + '/public';
-var showTasksFalse = require('./routes/tasks/handlers/showTasksFalse');
-var showTasksTrue = require('./routes/tasks/handlers/showTasksTrue');
-var insertTasks = require('./routes/tasks/handlers/insertTasks');
-var removeElement = require('./routes/tasks/handlers/removeElement');
-var updateAllTask = require('./routes/tasks/handlers/updateAllTask');
-var updateOneTask = require('./routes/tasks/handlers/updateOneTask');
 
 app.locals.moment = require('moment');
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use( express.static(publicFolder) )
 
+app.use(bodyParser.urlencoded({extended: true}));
 
-MongoClient.connect(url, function(err, db) {
+app.use( require('less-middleware')(path.join(__dirname, 'public')) );
+app.use( express.static(path.join(__dirname, 'public')) );
 
+var url = 'mongodb://localhost:27017/tasks';
 
-	app.get('/tasks', function(req,res) {
-		showTasksFalse(db,req,res);
-	})
-	app.post('/tasks', function(req,res) {
-		insertTasks(db,req,res);
+MongoClient.connect( url, function(err, db) {
+
+	app.use ('/', routerTasks(db) );
+
+	app.get ('/', function(req, res) {
 		res.redirect('/tasks');
+	} );
+
+	app.all('*', function(req, res){
+	  res.status(404).send();
 	})
-	app.get('/completed', function(req,res) {
-		showTasksTrue(db,req,res);
-	})
-	app.put('/tasks', function(req,res) {
-		var idTask = req.query.id;
-		if(idTask!==undefined){
-			updateOneTask(db,idTask,res);
-		}else{
-			updateAllTask(db,idTask,res);
-		}
-	})
-	app.delete('/tasks', function(req,res) {
-		removeElement(db,req,res);
-	})
-	app.listen(3000, function() {
-		console.log("Listening on port 3000");
-	})
-});
+
+	app.listen( PORT, function(){
+	  console.log('Express server listening on port ' + PORT);
+	});
+
+} )
